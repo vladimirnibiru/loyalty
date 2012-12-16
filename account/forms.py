f7e85e7b9cc10e96af743ee96afb4b35f8ab7240
vladimir <g00se1990@gmail.com>
2012-12-16 20:52:25 +0700

    signup

--- account/forms.py
+++ account/forms.py
@@ -1,11 +1,12 @@
+from random import randint
+
 from django import forms
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 
 
 class SigninForm(forms.Form):
-    username = forms.CharField(max_length=30, label=_('Member Number'),
-        widget=forms.TextInput)
+    username = forms.CharField(max_length=30, label=_('Member Number'))
     password = forms.CharField(max_length=128, widget=forms.PasswordInput)
 
     def clean(self):
@@ -21,3 +22,51 @@ class SigninForm(forms.Form):
             else:
                 self.user = u
                 return self.cleaned_data
+
+
+class SignupForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email', 'password')
+        widgets = {
+            'password': forms.PasswordInput,
+        }
+
+    email = forms.EmailField()
+    confirm_password = forms.CharField(max_length=128,
+        widget=forms.PasswordInput)
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if self.errors:
+            return email
+        try:
+            User.objects.get(email=email)
+        except User.DoesNotExist:
+            return email
+        else:
+            raise forms.ValidationError('This email already used')
+
+    def clean_confirm_password(self):
+        password = self.cleaned_data.get('password', '')
+        confirm_password = self.cleaned_data.get('confirm_password')
+        if self.errors:
+            return confirm_password
+        if password != confirm_password:
+            raise forms.ValidationError('Passwords doesn\'t match')
+        return confirm_password
+
+    def save(self, commit=True):
+        instance = super(SignupForm, self).save(commit=False)
+        instance.set_password(self.cleaned_data.get('password'))
+        while True:
+            instance.username = ''.join([str(randint(0, 10))\
+                for i in range(0, 16)])
+            try:
+                User.objects.get(username=instance.username)
+            except User.DoesNotExist:
+                break
+        if commit:
+            instance.save()
+        return instance
--- account/views.py
+++ account/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response, redirect
 from django.template.context import RequestContext
 
-from account.forms import SigninForm
+from account.forms import SigninForm, SignupForm
 
 
 def signin(request):
@@ -20,6 +20,14 @@ def signin(request):
 
 
 def signup(request):
+    if request.user.is_authenticated():
+        return redirect('account')
+
+    form = SignupForm(request.POST or None)
+    if form.is_valid():
+        user = form.save()
+        return render_to_response('account/signup_success.html', locals(),
+            context_instance=RequestContext(request))
     return render_to_response('account/signup.html', locals(),
         context_instance=RequestContext(request))
 
