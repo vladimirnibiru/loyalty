ede0ebf31e435bf22bd07a0544de3072a88f27bf
vladimir <g00se1990@gmail.com>
2012-12-16 16:06:47 +0700

    add dummy templates

--- loyalty/urls.py
+++ loyalty/urls.py
@@ -1,17 +1,8 @@
 from django.conf.urls import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'loyalty.views.home', name='home'),
-    # url(r'^loyalty/', include('loyalty.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', direct_to_template, {'template': 'home.html'}, name='home'),
+    url(r'^404/$', direct_to_template, {'template': '404.html'}, name='404'),
+    url(r'^500/$', direct_to_template, {'template': '500.html'}, name='500'),
 )
