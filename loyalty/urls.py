697996ff90efdf15909d4fc7c7d8a385b6e07705
vladimir <g00se1990@gmail.com>
2012-12-16 17:18:51 +0700

    add dummy css

--- loyalty/urls.py
+++ loyalty/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
@@ -6,3 +8,6 @@ urlpatterns = patterns('',
     url(r'^404/$', direct_to_template, {'template': '404.html'}, name='404'),
     url(r'^500/$', direct_to_template, {'template': '500.html'}, name='500'),
 )
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
