b0893f796157a143b8b3729b81bb6c1c50fea2b5
vladimir <g00se1990@gmail.com>
2012-12-18 19:13:20 +0700

    replace POST parametrs to JSON parametrs

--- transaction/api/views.py
+++ transaction/api/views.py
@@ -1,4 +1,4 @@
-from json import dumps
+from json import loads, dumps
 
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -10,21 +10,31 @@ log = getLogger(__name__)
 
 @csrf_exempt
 def credit_points(request):
-    form = MakeTransactionForm(False, data=request.POST)
+    if request.method != 'POST':
+        return HttpResponse(dumps({
+                'status': 'ERROR', 'error_message': 'POST-request only'
+            }), mimetype='application/json')
+    data = loads(request.body)
+    form = MakeTransactionForm(False, data=data)
     if form.is_valid():
         form.save()
-        log.info("Credit points ['OK'] POST %s" %dict(request.POST))
+        log.info("Credit points ['OK'] POST %s" %data)
         return HttpResponse(dumps({'status': 'OK'}),
             mimetype='application/json')
     else:
         errors = dict(form.errors.items())
-        log.info("Credit points ['ERROR'] POST %s ERRORS %s" %(dict(request.POST), errors))
+        log.info("Credit points ['ERROR'] POST %s ERRORS %s" %(data, errors))
         return HttpResponse(dumps({'status': 'ERROR',
             'error_message': errors}), mimetype='application/json')
 
 
 def query_points(request):
-    form = QueryPointsForm(request.GET)
+    if request.method != 'GET':
+        return HttpResponse(dumps({
+                'status': 'ERROR', 'error_message': 'GET-request only'
+            }), mimetype='application/json')
+    data = loads(request.body)
+    form = QueryPointsForm(data)
     if form.is_valid():
         return HttpResponse(dumps({'status': 'OK', 'points': form.user.get_profile().points}),
             mimetype='application/json')
@@ -35,14 +45,19 @@ def query_points(request):
 
 @csrf_exempt
 def debit_points(request):
-    form = MakeTransactionForm(True, data=request.POST)
+    if request.method != 'POST':
+        return HttpResponse(dumps({
+                'status': 'ERROR', 'error_message': 'POST-request only'
+            }), mimetype='application/json')
+    data = loads(request.body)
+    form = MakeTransactionForm(True, data=data)
     if form.is_valid():
         form.save()
-        log.info("Debit points ['OK'] POST %s" %dict(request.POST))
+        log.info("Debit points ['OK'] POST %s" %data)
         return HttpResponse(dumps({'status': 'OK'}),
             mimetype='application/json')
     else:
         errors = dict(form.errors.items())
-        log.info("Debit points ['ERROR'] POST %s ERRORS %s" %(dict(request.POST), errors))
+        log.info("Debit points ['ERROR'] POST %s ERRORS %s" %(data, errors))
         return HttpResponse(dumps({'status': 'ERROR',
             'error_message': errors}), mimetype='application/json')
